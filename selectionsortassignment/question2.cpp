1eb963ef2c76a11550d4d92370225822cf021c29
rohitsakhale <165694539+rohitsakhale@users.noreply.github.com>
2024-04-07 22:49:56 +0530

    Add files via upload

---
+++ selectionsortassignment/question2.cpp
@@ -0,0 +1,8 @@
+//Which of the following examples represent the worst case input for an insertion sort?
+//a) array in sorted order
+//b) large array
+//c) normal unsorted array
+//d) array sorted in reverse order
+
+
+//Answer - option d is correct 
---
+++ selectionsortassignment/question3.cpp
@@ -0,0 +1,8 @@
+//How many passes would be required during insertion sort to sort an array of 5 elements?
+//a) 1
+//b) Depends on order of elements
+//c) 4
+//d) 5
+
+
+//Answer -  option c is correct becaue n-1 passes are required 
